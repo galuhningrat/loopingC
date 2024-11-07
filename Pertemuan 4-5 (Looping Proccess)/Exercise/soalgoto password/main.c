13ded69ef9dc51437bb47aa722ccbf93ad14387e
galuhningrat <147510855+galuhningrat@users.noreply.github.com>
2024-11-07 13:42:22 +0700

    Update main.c

--- Pertemuan 4-5 (Looping Proccess)/Exercise/soalgoto password/main.c
+++ Pertemuan 4-5 (Looping Proccess)/Exercise/soalgoto password/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
+int main() {
+    char password[20];
+
+login:
+    printf("\nMasukkan password: ");
+    scanf("%s", password);
+
+    if(strcmp(password, "#admin123") != 0) {
+        printf("Password salah!\n");
+        goto login;
+    }
+
+    printf("Login berhasil!\n");
     return 0;
 }
